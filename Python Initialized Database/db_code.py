50a78a7fb247c9005559b9f12ff06b3d3ce33a7b
Jim <jimaudrei.malaza@gmail.com>
2024-06-22 23:19:32 +0800

    Folder Renamed to 'Python Initialized Database'

--- Python Initialized Database/db_code.py
+++ Python Initialized Database/db_code.py
@@ -3,14 +3,14 @@ import sqlite3
 import pandas as pd
 
 #Set the working directory here
-os.chdir(r'C:\Users\audre\OneDrive\Desktop\IBM Python Project\Python Scripts for Database Access')
+os.chdir(r'C:\Users\audre\OneDrive\Desktop\IBM Python Project\Python Initialized Database')
 
 conn = sqlite3.connect('STAFF.db')
 
 table_name = 'INSTRUCTOR'
 attribute_list = ['ID', 'First_Name', 'Last_Name', 'City', 'City_Code']
 
-file_path = r'C:\Users\audre\OneDrive\Desktop\IBM Python Project\Python Scripts for Database Access\INSTRUCTOR.csv'
+file_path = r'C:\Users\audre\OneDrive\Desktop\IBM Python Project\Python Initialized Database\INSTRUCTOR.csv'
 df = pd.read_csv(file_path, names = attribute_list)
 
 df.to_sql(table_name, conn, if_exists = 'replace', index = False)
